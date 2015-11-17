bd3788feee4eecf006d3b5faf0187ed16e70cdbd
fpcyan <fionapwconn@gmail.com>
2015-11-17 17:38:56 -0500

    Add model and associations for products categories productscategories and departments

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,2 +1,24 @@
 class Department < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true
+
+  validates :sub_departments, uniqueness: { scope: :parent_department,
+    message: "Parent department already contains this department" }
+
+
+
+  ## validate that parent departments do NOT have categories
+
+
+  # f_0
+  belongs_to :parent_department, inverse_of: :sub_departments
+  has_many :categories, inverse_of: :sub_department
+  has_many :sub_departments, inverse_of: :parent_department
+
+  # f_1
+  has_many :categories, through: :sub_department, source: :categories
+
+  # f_2
+  has_many :products, through: :categories, source: :products
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -16,3 +16,28 @@ User.create!(first_name: "Fiona", last_name: "Conn", email: "fiona@example.com",
     password:   "password"
   )
 end
+
+25.times do
+  Product.create!(
+    product_name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price * 100,
+    description: Faker::Lorem.paragraph,
+    specs: Faker::Lorem.paragraph,
+    quantity: rand(0...100)
+  )
+end
+
+# parent_departments = []
+# 5.times do
+#   title = Faker::Commerce.department
+#   departments << Department.create!(title: title)
+#
+# sub_departments = []
+#
+# 10.times do
+#   section = Faker::Commerce.department
+#   Category.create!(
+#     section: section,
+#
+#   )
+end
