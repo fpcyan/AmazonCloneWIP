e339c54a5109fb6f715461ed5b87e1de09bbc5bc
fpcyan <fionapwconn@gmail.com>
2015-11-17 15:06:49 -0500

    Change dept id to category id on productcategory join table. add category model. pre migration

--- db/migrate/20151117195901_create_products_categories.rb
+++ db/migrate/20151117195901_create_products_categories.rb
@@ -2,11 +2,11 @@ class CreateProductsCategories < ActiveRecord::Migration
   def change
     create_table :products_categories do |t|
       t.integer :product_id, null: false
-      t.integer :department_id, null: false
+      t.integer :category_id, null: false
 
       t.timestamps null: false
     end
     add_index :products_categories, :product_id
-    add_index :products_categories, :department_id
+    add_index :products_categories, :category_id
   end
 end
