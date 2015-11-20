31df50e203ee8da42cd20d8f157796cdf13731df
fpcyan <fionapwconn@gmail.com>
2015-11-20 18:46:32 -0500

    Add image height and width to jbuilder

--- app/assets/javascripts/components/products/featured_item.js.jsx
+++ app/assets/javascripts/components/products/featured_item.js.jsx
@@ -5,7 +5,7 @@ var FeaturedItem = React.createClass({
   render: function () {
     return(
       <li className="carousel-card">
-        <img className="product-image" src={this.props.item.image[0].image_url}/>
+        <img className="product-image" src={this.props.item.image[0].url}/>
         <p className="product-details-button">More Details</p>
       </li>
     );
--- app/views/api/products/_image.json.jbuilder
+++ app/views/api/products/_image.json.jbuilder
@@ -1,4 +1,6 @@
 
 
 json.extract! image, :id, :alt, :main_image
-json.image_url asset_path(image.image.url(:carousel))
+json.url asset_path(image.image.url(:carousel))
+json.width image.image.width(:carousel)
+json.height image.image.height(:carousel)
