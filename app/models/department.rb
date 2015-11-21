59e897084ea0aebde5e4325974da13168b278713
fpcyan <fionapwconn@gmail.com>
2015-11-21 17:23:53 -0500

    Add and remove model validation

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,12 +3,12 @@ class Department < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: true
 
-
   belongs_to :parent_department, inverse_of: :sub_departments, class_name: "Department", foreign_key: :parent_dept_id
   has_many :sub_departments, inverse_of: :parent_department, class_name: "Department", foreign_key: :parent_dept_id
 
   has_many :categories, inverse_of: :sub_department, class_name: "Category", foreign_key: :department_id
   has_many :products, through: :categories, source: :products
+
 
 
   def self.featured_products ## finds the 2 subdepartments with the most products
