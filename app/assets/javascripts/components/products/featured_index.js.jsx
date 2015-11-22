ad57452158d586552306491e55e4c7eb37dd2944
fpcyan <fionapwconn@gmail.com>
2015-11-22 18:11:27 -0500

    Transferr Jquery carousel animation code into featureindex react component. Not implemented

--- app/assets/javascripts/components/products/featured_index.js.jsx
+++ app/assets/javascripts/components/products/featured_index.js.jsx
@@ -16,7 +16,7 @@ var FeaturedIndex = React.createClass({
     });
     return (
         <div className="desktop-wrapper">
-          <div className="featured">
+          <div className="featured" id={this.props.feature.title.toLowerCase()}>
             <h2 className="featured-dept"><a href="#" className="dept-link">{this.props.feature.title}</a></h2>
 
             <button className="arrow-button slide-left" onClickCapture={this.handleButtonClick}>
@@ -51,5 +51,74 @@ var FeaturedIndex = React.createClass({
   handleProductClick: function (e) {
 
   },
+
+// saving for later. done with carousel attemps for now
+  jQVars: function () {
+    var el = $("#" + this.props.feature.title.toLowerCase());
+    var pics = el.find(".carousel-card");
+    var viewPort = el.find(".carousel-viewport");
+    var viewWidth = el.find(".carousel").width();
+    var leftAnchorIdx = 0;
+    var max = viewPort.width();
+    var maxDisplacement = -1 * (max - viewWidth);
+  },
+
+  jQMoveViewPort: function (dir) {
+    var currPosition = viewPort.position().left;
+    var displace = dir * viewWidth + currPosition;
+    if (dir === -1) {
+      this.slideRight(displace);
+    } else if (dir === 1) {
+      this.slideLeft(displace);
+    }
+  },
+
+  jQslideLeft: function(displace) {
+    if (displace >= 0) {
+      displace = 0;
+      $(".slide-left").addClass("disabled");
+    }
+      if ($(".slide-right").hasClass("disabled")) {
+      $(".slide-right").removeClass("disabled");
+    }
+    var sum = 0;
+    var nextIdx = anchorIdx - 1;
+    var offSet;
+    while (true) {
+      sum += this.state.products[nextIdx].image[0].width;
+      // only works if you only ever click the left button
+      // need to check anchor based on direction to get either to work
+      if (sum > viewWidth) {
+        anchorIdx = nextIdx;
+        offSet = viewWidth - sum + this.state.products[nextIdx].image[0].width;
+        break;
+      }
+      nextIdx++;
+    }
+    viewPort.css("left", displace - offSet );
+  },
+
+  jQslideRight: function(displace) {
+    if (displace <= maxDisplacement) {
+      displace = maxDisplacement;
+      $(".slide-right").addClass("disabled");
+    }
+      if ($(".slide-left").hasClass("disabled")) {
+      $(".slide-left").removeClass("disabled");
+    }
+    var sum = 0;
+    var nextIdx = leftAnchorIdx;
+    var offSet;
+    while (true) {
+      sum += this.state.products[nextIdx].image[0].width;
+      if (sum > viewWidth) {
+        this.anchorIdx = nextIdx;
+        offSet = viewWidth - sum + this.state.products[nextIdx].image[0].width;
+        break;
+      }
+      nextIdx++;
+    }
+    viewPort.css("left", displace + offSet );
+  }
 
 });
