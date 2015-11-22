ffcf7785805e3ae8d09df156bc0d0a5abcd16721
fpcyan <fionapwconn@gmail.com>
2015-11-22 16:14:55 -0500

    Update FeatureItem component html

--- app/assets/javascripts/components/products/featured_item.js.jsx
+++ app/assets/javascripts/components/products/featured_item.js.jsx
@@ -1,14 +1,13 @@
 var FeaturedItem = React.createClass({
 
   render: function () {
-    var klass = (
-      "product-image " +
-      this.props.feature
-    );
+
     return(
       <li className="carousel-card">
-        <img id={this.props.feature} className={klass} src={this.props.item.url}/>
-        <p className="product-details-button">More Details</p>
+        <a href="#" className="feature-link">
+          <img className="feature-image" src={this.props.item.url}/>
+          <p className="feature-details-button">More Details</p>
+        </a>
       </li>
     );
   }
