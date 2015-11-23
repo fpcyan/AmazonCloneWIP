6a2ce0aaac35072a86d155ba02a0e6ca56e40bb0
fpcyan <fionapwconn@gmail.com>
2015-11-23 14:58:05 -0500

    small style additions. refactor nav bar css to remove float

--- app/assets/javascripts/components/forms/product_mgmt_form.js.jsx
+++ app/assets/javascripts/components/forms/product_mgmt_form.js.jsx
@@ -5,12 +5,17 @@ var ProductMgmtForm = React.createClass({
     return (
       <div className="mgmt-box-inner">
         <form className="mgmt-form">
+          <span className="product-price-summary">
+            <strong className="product-price">$153.00 {this.props.product.price}</strong>
+              <strong className="free-shipping">FREE Shipping! </strong><a href="#" className="shipping-details normal-link">Details</a>
+          </span>
           <label className="quantity-dropdown">
             Qty:
             <select className="dropdown">
             </select>
           </label>
 
+          <span className="same-day-delivery"><a href="#">Time Travel Delivery</a></span>
           <button className="cart-button">
             <img className="cart-button-img" />
             <p className="cart-button-txt">Add to Cart</p>
--- app/assets/javascripts/components/products/show_product.js.jsx
+++ app/assets/javascripts/components/products/show_product.js.jsx
@@ -1,7 +1,7 @@
 var ShowProduct = React.createClass({
 
   getInitialState: function () {
-    return ({ product: { product_name: "", price: 0, stock: 100, images: []} });
+    return ({ product: { id: "", product_name: "", price: 0, stock: 100, images: []} });
   },
 
   render: function () {
@@ -31,19 +31,15 @@ var ShowProduct = React.createClass({
           <div className="product-specs-wrapper">
             <h1 className="product-name">I am a Food Product 2000 Triple X AMA{this.state.product.product_name}</h1>
             <span className="review-summary">Placeholder text</span>
-            <span className="product-price-summary">
-              <strong className="product-price">$153.00 {this.state.product.price}</strong>
-                <strong className="free-shipping">FREE Shipping! </strong><a href="#" className="shipping-details normal-link">Details</a>
-            </span>
+
             <span className="stock">{this.state.product.stock}</span>
-            <span className="same-day-delivery"><a href="#">Time Travel Delivery</a></span>
 
             { specs }
 
           </div>
 
           <div className="form-outer-box">
-            <ProductMgmtForm />
+            <ProductMgmtForm product={this.state.product} />
           </div>
 
         </section>
