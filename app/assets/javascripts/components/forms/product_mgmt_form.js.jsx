87ba6a2d42547994ebca7a9f2c0b7626c27608db
fpcyan <fionapwconn@gmail.com>
2015-11-23 23:00:34 -0500

    style mgmt form on product show page

--- app/assets/javascripts/components/forms/product_mgmt_form.js.jsx
+++ app/assets/javascripts/components/forms/product_mgmt_form.js.jsx
@@ -1,25 +1,59 @@
 var ProductMgmtForm = React.createClass({
 
+  getInitialState: function () {
+    return ({ qty: 1 });
+  },
+
+  handlePlus: function (e) {
+    e.preventDefault();
+    if (this.state.qty + 1 <= 5) {
+      this.setState({ qty: this.state.qty + 1 });
+    }
+  },
+
+  handleMinus: function (e) {
+    e.preventDefault();
+    if (this.state.qty - 1 >= 0) {
+    this.setState({ qty: this.state.qty - 1 });
+    }
+  },
+
   render: function () {
+
+    var qtyMinusClass = "js-qty-enabled";
+    var qtyPlusClass = "js-qty-enabled";
+    if (this.state.qty === 0) {
+      qtyMinusClass = "js-qty-disabled";
+    } else if (this.state.qty === 5) {
+      qtyPlusClass = "js-qty-disabled";
+    }
 
     return (
       <div className="mgmt-box-inner">
         <form className="mgmt-form">
           <span className="product-price-summary">
             <strong className="product-price">${(this.props.product.price / 100).toFixed(2)}</strong>
+
               <strong className="free-shipping">FREE Shipping! </strong><a href="#" className="shipping-details normal-link">Details</a>
           </span>
-          <label className="quantity-dropdown">
-            Qty:
-            <select className="dropdown">
-            </select>
-          </label>
+          <span className="same-day-delivery"><a href="#">Time Travel Delivery: Order yesterday!</a></span>
 
-          <span className="same-day-delivery"><a href="#">Time Travel Delivery</a></span>
-          <button className="cart-button">
-            <img className="cart-button-img" />
-            <p className="cart-button-txt">Add to Cart</p>
-          </button>
+          <div className="cart-wrapper">
+            <div className="qty-button-wrapper">
+              <button className={"i-minus " + qtyMinusClass}
+                onClick={this.handleMinus}>
+                <img title="minus one quantity" src={appImages.minusSign}/>
+              </button>
+              <div className="quantity-indicator">{this.state.qty}</div>
+              <button className={"i-plus " + qtyPlusClass} onClick={this.handlePlus}>
+                <img title="plus one quantity" src={appImages.plusSign} />
+              </button>
+            </div>
+
+            <button className="cart-button">
+              <p className={"cart-button-txt " + qtyMinusClass}>Add to Cart</p>
+            </button>
+          </div>
 
           <button className="wish-list-button">Add to Wish List</button>
         </form>
