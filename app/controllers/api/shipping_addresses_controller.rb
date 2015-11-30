f20dd53f34182a4f84cc211ddacc10e4e2486406
fpcyan <fionapwconn@gmail.com>
2015-11-29 21:00:01 -0500

    create json view for shipping addresses

--- app/controllers/api/shipping_addresses_controller.rb
+++ app/controllers/api/shipping_addresses_controller.rb
@@ -1,6 +1,8 @@
 class Api::ShippingAddresses < Api::ApiController
 
   def index
+    @shipping_addresses = current_user.shipping_addresses.load
+    render "api/users/" + current_user.id + "/shipping_addresses"
   end
 
   def create
---
+++ app/views/api/shipping_addresses/index.json.jbuilder
@@ -0,0 +1,2 @@
+
+json.extract!(@shipping_addresses, :id, :full_name, :address_one, :address_two, :city, :region, :zip, :phone_number)
