c691a6a57f1367a5b54069606084097e635783f2
fpcyan <fionapwconn@gmail.com>
2015-11-29 20:33:18 -0500

    add shipping model, pre migration

---
+++ db/migrate/20151130012955_create_create_shipping_addresses.rb
@@ -0,0 +1,17 @@
+class CreateShippingAddresses < ActiveRecord::Migration
+  def change
+    create_table :create_shipping_addresses do |t|
+      t.integer :user_id, null: false
+      t.string :full_name, null: false
+      t.string :address_one, null: false
+      t.string :address_two
+      t.string :city, null: false
+      t.string :region, null: false
+      t.integer :zip, null: false
+      t.integer :phone_number
+
+      t.timestamps null: false
+    end
+    add_index :create_shipping_addresses, :user_id
+  end
+end
