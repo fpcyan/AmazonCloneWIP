e422ec5ccb294c70933bb0572dcfdee7fdbb3f51
fpcyan <fionapwconn@gmail.com>
2015-11-29 21:25:32 -0500

    add address store listeners to checkout route

--- app/assets/javascripts/components/checkout/checkout.js.jsx
+++ app/assets/javascripts/components/checkout/checkout.js.jsx
@@ -1,6 +1,18 @@
 var Checkout = React.createClass({
 
+  getInitialState: function () {
+    return ({ cart: CartStore.all(), shippingAddresses: [], payments: [] });
+  },
 
+  componentDidMount: function () {
+    CheckoutApiUtils.fetchShippingAddresses(CurrentUserStore.currentUser().id);
+    ShippingAddressStore.addChangeListener(this.onAddressChange);
+  },
+
+  componentWillUnmount: function () {
+    ShippingAddressStore.removeChangeListener(this.onAddressChange);
+  },
+  
   render: function () {
 
     return (
