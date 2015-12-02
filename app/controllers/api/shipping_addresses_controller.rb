ed678d0d636432264a743e8267aaeb10a9988949
fpcyan <fionapwconn@gmail.com>
2015-12-01 23:04:01 -0500

    add create and destroy methods to shipping address controller

--- app/controllers/api/shipping_addresses_controller.rb
+++ app/controllers/api/shipping_addresses_controller.rb
@@ -1,15 +1,35 @@
 class Api::ShippingAddressesController < Api::ApiController
 
   def index
-    @shipping_addresses = current_user.shipping_addresses.load
     render "api/shipping_addresses/index"
   end
 
   def create
+    new_address = current_user.shipping_addresses.new(address_params)
+
+    if new_address.save
+      render "api/shipping_addresses/index"
+    else
+      render json: { errors: new_address.errors.full_messages, status: 422 }
+    end
   end
 
   def destroy
+    address = ShippingAddresses.find(params[:shippingId])
+    address.destroy if current_user.shipping_addresses.include?(address)
+
+    render "api/shipping_addresses/index"
   end
 
+  private
+
+    def address_params
+      params
+        .require(:address)
+        .permit(
+          :full_name, :address_one, :address_two,
+          :city, :region, :zip, :phone_number
+          )
+    end
 
 end
--- app/views/api/shipping_addresses/index.json.jbuilder
+++ app/views/api/shipping_addresses/index.json.jbuilder
@@ -1,3 +1,3 @@
-json.array! @shipping_addresses do |shipping_address|
+json.array! current_user.shipping_addresses.order(updated_at: :desc) do |shipping_address|
     json.extract!( shipping_address, :id, :full_name, :address_one, :address_two, :city, :region, :zip, :phone_number)
 end
