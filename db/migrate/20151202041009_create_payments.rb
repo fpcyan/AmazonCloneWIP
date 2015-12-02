0c023d2ef91d86f4e6cf33222a2143339015b1ce
fpcyan <fionapwconn@gmail.com>
2015-12-01 23:50:59 -0500

    migrated payments

--- db/migrate/20151202041009_create_payments.rb
+++ db/migrate/20151202041009_create_payments.rb
@@ -1,10 +1,11 @@
 class CreatePayments < ActiveRecord::Migration
   def change
     create_table :payments do |t|
-      t.integer :user_id
-      t.string :full_name
-      t.datetime :expiration
-      t.string :cc_token
+      t.integer :user_id,     null: false
+      t.string :full_name,    null: false
+      t.datetime :expiration, null: false
+      t.string :cc_token,     null: false
+      t.string :merchant,     null: false
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151130012955) do
+ActiveRecord::Schema.define(version: 20151202041009) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,18 @@ ActiveRecord::Schema.define(version: 20151130012955) do
     t.datetime "image_updated_at"
     t.text     "image_meta"
   end
+
+  create_table "payments", force: :cascade do |t|
+    t.integer  "user_id",    null: false
+    t.string   "full_name",  null: false
+    t.datetime "expiration", null: false
+    t.string   "cc_token",   null: false
+    t.string   "merchant",   null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "payments", ["user_id"], name: "index_payments_on_user_id", using: :btree
 
   create_table "products", force: :cascade do |t|
     t.string   "product_name", null: false
