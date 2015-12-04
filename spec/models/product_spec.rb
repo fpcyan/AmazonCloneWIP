75c3a73e19a3903c2aef5d80edc40591f61b5e15
fpcyan <fionapwconn@gmail.com>
2015-12-04 11:14:47 -0500

    began to setup rspec tests for models

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,7 +1,11 @@
-require_relative '../../app/models/product.rb'
-describe Product do
+require 'rails_helper'
+
+RSpec.describe Product, :type => :model do
+  let(:category) do
+    Category.new(section: "nip")
+  end
   let(:subject) do
-    Product.new(
+    category.products.new(
       product_name: "cat food",
       price: 100000,
       description: "it's... cat food",
@@ -11,12 +15,20 @@ describe Product do
   end
 
   describe "#already_exists" do
+    let (:subject_two) do
+      category.products.new(
+      product_name: "cat food",
+      price: 100000,
+      description: "it's... cat food",
+      specs: "10lbs 5oz",
+      quantity: 5
+      )
+    end
 
     context 'when the category already has the product' do
       it "returns nil" do
-        except { subject.already_exists }.to be_nil
+        expect subject_two.already_exists(category).to be_nil
       end
     end
   end
-
 end
