727db29df875583b1cfcd0ef2ac3ff15e7afdd66
fpcyan <fionapwconn@gmail.com>
2015-12-04 13:26:14 -0500

    Finish validations and associations for user, user order, and order models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,8 @@
 class Order < ActiveRecord::Base
-  validates :product_id, :price, :quantity, :stripe_charge_id, :refunded, presence: true
+  validates :product_id, :price, :quantity, :refunded, :user_orders_id, presence: true
   validates :refunded, inclusion: { in: [true, false] }
   validates :quantity, numericality: { greater_than: 0,
     message: "Quantity must be at least one" }
 
-  belongs_to :user_order, inverse_of: :orders 
+  belongs_to :user_order, inverse_of: :orders
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,5 +1,5 @@
 class UserOrder < ActiveRecord::Base
-  validates :user_id, :order_id, presence: true
+  validates :user_id, :purchase_total, :stripe_charge_id, presence: true
 
   belongs_to :user, inverse_of: :user_orders
   has_many :orders, inverse_of: :user_order
