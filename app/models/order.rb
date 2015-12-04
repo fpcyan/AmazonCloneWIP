1f8c8ba58acb5c10cffacf7ee79da98ca2c0fbea
fpcyan <fionapwconn@gmail.com>
2015-12-04 12:55:35 -0500

    add validations for quantity and refunded in order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ActiveRecord::Base
-  validates :product_id, :price, :quantity, :stripe_charge_id, presence: true
+  validates :product_id, :price, :quantity, :stripe_charge_id, :refunded, presence: true
+  validates :refunded, inclusion: { in: [true, false] }
+  validates :quantity, numericality: { greater_than: 0,
+    message: "Quantity must be at least one" }
+
+  
 end
